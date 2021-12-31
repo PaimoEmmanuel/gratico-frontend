9e5ad4a8e88c5a3c45401ab9007bb95309aca79c
unknown <aranwaoluwa@gmail.com>
2021-12-31 22:59:53 +0100

    corrected stuff

--- src/components/organisms/landing-view.tsx
+++ src/components/organisms/landing-view.tsx
@@ -15,12 +15,11 @@ const Container = styled.div`
 	align-items: center;
 	overflow: hidden;
 	text-align: center;
-	position: relative;
-	// z-index: 105;
+	position: fixed;
+	z-index: 105;
 	// top: 0;
 	// right: 0;
 `
-
 const Text = styled.p`
 	margin-top: 16px;
 	font-size: 18px;
@@ -29,31 +28,19 @@ const Text = styled.p`
 		display: inline-block;
 		overflow: hidden;
 		opacity: 1;
-		// width: auto;
-		&:first-child {
-			width: 49px;
-		}
-		&:nth-child(2) {
-			width: 75px;
-		}
-		&:last-child {
-			width: 108px;
-		}
+		width: auto;
 	}
 `
-
 const LogoWrap = styled.div`
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
 `
-
 const LineWrap = styled.div`
 	margin-top: -10px;
 	position: relative;
 `
-
 const LineWrapAfter = styled.div`
 	position: absolute;
 	right: 0;
@@ -67,7 +54,6 @@ const LandingView: React.FC<{}> = () => {
 	const underlineAfterRef = useRef(null)
 	const logoWrapRef = useRef(null)
 	const containerRef = useRef(null)
-
 	useEffect(() => {
 		const span = document.querySelectorAll('span')
 		gsap.timeline()
@@ -96,10 +82,29 @@ const LandingView: React.FC<{}> = () => {
 				width: 0,
 				opacity: 0,
 			})
-			.to(containerRef.current, {
-				duration: 0.1,
+			.from(span[3], {
+				duration: 1,
 				ease: 'power2',
-				position: 'relative',
+				width: 0,
+				opacity: 0,
+			})
+			.from(span[4], {
+				duration: 1,
+				ease: 'power2',
+				width: 0,
+				opacity: 0,
+			})
+			.from(span[5], {
+				duration: 1,
+				ease: 'power2',
+				width: 0,
+				opacity: 0,
+			})
+			.from(span[6], {
+				duration: 1,
+				ease: 'power2',
+				width: 0,
+				opacity: 0,
 			})
 			.to(containerRef.current, {
 				duration: 0.7,
@@ -108,8 +113,12 @@ const LandingView: React.FC<{}> = () => {
 				minHeight: 0,
 				height: 0,
 			})
+			.to(containerRef.current, {
+				duration: 0.7,
+				ease: 'power2',
+				position: 'relative',
+			})
 	}, [])
-
 	return (
 		<Container ref={containerRef}>
 			<LogoWrap ref={logoWrapRef} className='container'>
@@ -120,9 +129,9 @@ const LandingView: React.FC<{}> = () => {
 				</LineWrap>
 			</LogoWrap>
 			<Text>
-				<span>Share</span> <span>beautiful</span> <span>experiences.</span>
-				{/* <br /> */}
-				{/* <span> for</span> <span>all</span> <span>to</span> <span>read.</span> */}
+				<span>Share</span> <span>faithbased</span> <span>content</span>
+				<br />
+				<span> for</span> <span>all</span> <span>to</span> <span>read.</span>
 			</Text>
 		</Container>
 	)
