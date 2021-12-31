0f807d9fbd319317908994a449bf4a495c8b4ae7
PaimoEmmanuel <paimoemmanuel2016@gmail.com>
2021-12-31 18:43:11 +0100

    quick fix

--- src/router/app-router.tsx
+++ src/router/app-router.tsx
@@ -1,24 +1,32 @@
-import { Router, Switch, Route, Redirect, withRouter } from 'react-router-dom'
-import { createBrowserHistory } from 'history'
-import { useContext, useEffect, useState } from 'react'
+import { Router, Switch, Route, Redirect, withRouter } from "react-router-dom";
+import HomePage from "../pages/index";
+import { createBrowserHistory } from "history";
+import { useContext, useEffect, useState } from "react";
+import WriteStory from "../pages/write-story";
+import ScrollToTop from "../components/molecules/scroll";
+import Explore from "../pages/explore";
 
-import HomePage from '../pages/index'
-import WriteStory from '../pages/write-story'
-import Explore from '../pages/explore'
+const history = createBrowserHistory();
 
-const history = createBrowserHistory()
 const AppRouter = () => {
-	return (
-		<Router history={history}>
-			<Switch>
-				<Route path='/write-story' component={WriteStory} exact={true} />
-				<Route path='/explore' component={Explore} exact={true} />
-				<Route path='/story/:storyID' component={WriteStory} exact={true} />
-				<Route path='/' component={HomePage} exact={true} />
-				<Redirect to='/not-found' />
-			</Switch>
-		</Router>
-	)
-}
+  return (
+    <Router history={history}>
+      <>
+        <ScrollToTop />
+        <Switch>
+          <Route
+            path="/write-story"
+            component={withRouter(WriteStory)}
+            exact={true}
+          />
+          <Route path='/explore' component={Explore} exact={true} />
+          <Route path="/stroy/:storyID" component={WriteStory} exact={true} />
+          <Route path="/" component={HomePage} exact={true} />
+          <Redirect to="/not-found" />
+        </Switch>
+      </>
+    </Router>
+  );
+};
 
 export default AppRouter
