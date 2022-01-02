a8f8998aa1562a72fb9c7a6e43e0de8745228fc6
unknown <aranwaoluwa@gmail.com>
2022-01-03 00:31:35 +0100

    fixed progress bar

--- src/pages/write-story.tsx
+++ src/pages/write-story.tsx
@@ -1,12 +1,12 @@
-import { useContext, useState } from "react";
-import styled from "styled-components";
-import BaseNote from "../components/molecules/base-note";
-import Navigation from "../components/molecules/nav";
-import CoverPhoto from "../components/organisms/cover-photo";
-import Footer from "../components/organisms/footer";
-import StoryDetails from "../components/organisms/story-details";
-import WriterDetails from "../components/organisms/writer-details";
-import { StoryContext } from "../contexts/write-story-context";
+import { useContext, useState } from 'react'
+import styled from 'styled-components'
+import BaseNote from '../components/molecules/base-note'
+import Navigation from '../components/molecules/nav'
+import CoverPhoto from '../components/organisms/cover-photo'
+import Footer from '../components/organisms/footer'
+import StoryDetails from '../components/organisms/story-details'
+import WriterDetails from '../components/organisms/writer-details'
+import { StoryContext } from '../contexts/write-story-context'
 
 // FDFAF2
 const Content = styled.div`
@@ -40,70 +40,65 @@ const ProgressSpan = styled.span<{ active?: boolean }>`
 `
 
 const WriteStory: React.FC = () => {
-  const [activeView, setActiveView] = useState(0);
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [image, setImage] = useState("");
-  const { story, setStory } = useContext(StoryContext);
+	const [activeView, setActiveView] = useState(0)
+	const [email, setEmail] = useState('')
+	const [name, setName] = useState('')
+	const [title, setTitle] = useState('')
+	const [content, setContent] = useState('')
+	const [image, setImage] = useState('')
+	const { story, setStory } = useContext(StoryContext)
 
-  const handlePreview = () => {
-    const date = new Date().toString().slice(4, 10);
-    setStory({ ...story, email, name, title, content, image, date });
-  };
-  return (
-    <div>
-      <Navigation />
-      <Content>
-        <HeadingWrap>
-          <Title>
-            Remember, your gratitude story could encourage someone out there.
-          </Title>
-          <Text>
-            No long sign up forms! Ready to spread gratitude in the air? Let’s
-            do it!
-          </Text>
-          <ProgressBar>
-            <ProgressSpan active={activeView == 0} />
-            <ProgressSpan active={activeView == 1} />
-            <ProgressSpan active={activeView == 2} />
-            <ProgressSpan />
-          </ProgressBar>
-        </HeadingWrap>
-        <div>
-          {activeView === 0 && (
-            <WriterDetails
-              email={email}
-              setEmail={setEmail}
-              name={name}
-              setName={setName}
-              onSubmit={setActiveView}
-            />
-          )}
-          {activeView === 1 && (
-            <StoryDetails
-              title={title}
-              setTitle={setTitle}
-              onSubmit={setActiveView}
-              content={content}
-              setContent={setContent}
-            />
-          )}
-          {activeView === 2 && (
-            <CoverPhoto
-              onSubmit={setActiveView}
-              image={image}
-              setImage={setImage}
-              handlePreview={handlePreview}
-            />
-          )}
-        </div>
-        <BaseNote />
-      </Content>
-      <Footer />
-    </div>
-  );
-};
+	const handlePreview = () => {
+		const date = new Date().toString().slice(4, 10)
+		setStory({ ...story, email, name, title, content, image, date })
+	}
+	return (
+		<div>
+			<Navigation />
+			<Content>
+				<HeadingWrap>
+					<Title>Remember, your gratitude story could encourage someone out there.</Title>
+					<Text>No long sign up forms! Ready to spread gratitude in the air? Let’s do it!</Text>
+					<ProgressBar>
+						<ProgressSpan active={activeView >= 0} />
+						<ProgressSpan active={activeView >= 1} />
+						<ProgressSpan active={activeView >= 2} />
+						<ProgressSpan />
+					</ProgressBar>
+				</HeadingWrap>
+				<div>
+					{activeView === 0 && (
+						<WriterDetails
+							email={email}
+							setEmail={setEmail}
+							name={name}
+							setName={setName}
+							onSubmit={setActiveView}
+						/>
+					)}
+					{activeView === 1 && (
+						<StoryDetails
+							title={title}
+							setTitle={setTitle}
+							onSubmit={setActiveView}
+							content={content}
+							setContent={setContent}
+						/>
+					)}
+					{activeView === 2 && (
+						<CoverPhoto
+							onSubmit={setActiveView}
+							image={image}
+							setImage={setImage}
+							handlePreview={handlePreview}
+						/>
+					)}
+				</div>
+				<BaseNote />
+			</Content>
+			<Footer />
+		</div>
+	)
+}
 
 export default WriteStory
