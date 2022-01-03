613451e091b3f52d89ea037fd3b4eac5e26b5a75
PaimoEmmanuel <paimoemmanuel2016@gmail.com>
2022-01-03 14:28:07 +0100

    add base url

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export const request = axios.create({
-	baseURL: process.env.REACT_APP_BASE_URL,
+	// baseURL: process.env.REACT_APP_BASE_URL,
+	baseURL: 'https://api.gratico.xyz/api',
 	// withCredentials: true,
 	timeout: 60000,
 	headers: {
