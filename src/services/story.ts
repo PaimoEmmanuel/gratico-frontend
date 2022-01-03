30d9daa1340e48371b65969264ea78f2a55bfce2
PaimoEmmanuel <paimoemmanuel2016@gmail.com>
2022-01-03 03:29:14 +0100

    share edit link functionality

--- src/services/story.ts
+++ src/services/story.ts
@@ -3,13 +3,16 @@ import { request } from '.'
 export const getAllStories = (pageNumber: number, filterBy?: 'popular' | 'newest' | 'oldest') => {
 	return request.get(`/stories?page=${pageNumber}&filter=${filterBy && filterBy}`)
 }
-// http://api.gratico.xyz/api/stories?page=1&filter=oldest
+
 export const getOneStory = (storyId: string) => {
 	return request.get(`/stories/${storyId}`)
 }
 
 export const getStoryFromToken = (token: string) => {
 	return request.get(`/tokens/${token}/story`)
+}
+export const sendEditStoryLink = (email: string) => {
+	return request.post(`/user/email/stories`, { email })
 }
 
 export const likeStory = (storyId: string, userToken: string) => {
